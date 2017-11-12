da6f6f9172218a7fe002da4231c57b66b837fcf8
zhanghengyu <18810003053@sina.cn>
2017-11-12 14:05:41 +0800

    增加分页

--- src/main/java/com/hy/web/server/controller/ServerController.java
+++ src/main/java/com/hy/web/server/controller/ServerController.java
@@ -1,7 +1,13 @@
 package com.hy.web.server.controller;
 
+import com.hy.entity.Page;
+import com.hy.entity.User;
+import com.hy.web.server.service.IServerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author zhanghengyu
@@ -13,9 +19,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/server")
 public class ServerController {
 
-    @RequestMapping("/list")
-    public String toList(){
+    @Autowired
+    private IServerService service;
 
-        return "list/list";
+    @RequestMapping("/list")
+    public ModelAndView toList(@RequestParam(value="index",defaultValue ="1")int index, @RequestParam(value="page",defaultValue = "10")int pageSize){
+        ModelAndView view = new ModelAndView("list/list");
+        Page<User> page = new Page<User>();
+        page.setCurrentIndex(index);
+        page.setPageSize(pageSize);
+        service.getPage(page);
+        view.addObject("page",page);
+        return view;
     }
 }
